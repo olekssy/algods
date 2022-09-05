3e9fdad73f2f0fbf6c3bcb583b82f94a8d242a6d
Olekssy Lialka <dev.lialka@pm.me>
2022-09-05 17:07:42 +0200

    Added merge sorted LL, refactored LL class

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -1,73 +1,76 @@
 """ Implementation of the linked list alike data structurers. """
 
-from typing import Optional, Union
+from typing import Optional
 
 
-class ListNode:
+class Node:
     """ Singly linked list node. """
 
-    def __init__(self,
-                 key: Optional[Union[int, float, str]] = None,
-                 val: Optional[any] = None,
-                 _next=None) -> None:
-        self.key = key
+    def __init__(self, val: Optional[int] = None, next=None) -> None:
         self.val = val
-        self.next = _next
+        self.next = next
 
 
 class LinkedList:
     """ Singly linked list.
 
     Attributes:
-        head (ListNode, None): a head node of the linked list.
+        head (Node, None): a head node of the linked list.
         size (int): number of nodes in the linked list.
 
     Methods:
         clear(): resets list to the initial (empty) state.
-        add(key, val, index): inserts a key-value node at the index, if valid.
+        add(val, index): inserts a value node at the index, if valid.
         remove(index): removes node at the index, if valid.
-        get(index): gets key-value of the node at the index, if valid.
-        find(key): finds index of the node matching key, if exists.
-        to_list(): gets all key-value pairs as a list.
+        get(index): gets value of the node at the index, if valid.
+        find(val): finds index of the node matching value, if exists.
+        to_list(): gets all values as an array.
         rotate(k): rotate list by k places.
         reverse(): reverses linked list inplace.
         is_palindrom(): checks if linked list values form a palidrom.
     """
 
     def __init__(self) -> None:
-        self.head: Optional[ListNode] = self.clear()
-        self.size: int = 0
+        self.head: Optional[Node] = self.clear()
+        self.size: int
 
     def clear(self) -> None:
         """ Resets list to the initial (empty) state. """
 
         self.head = None
-        self.size = 0
 
-    def add(self,
-            key: Optional[Union[int, float, str]],
-            val: Optional[any] = None,
-            index: int = 0) -> None:
-        """ Inserts a key-val node at the index, if valid. """
+    @property
+    def size(self) -> int:
+        """ Number of nodes in the list. """
+
+        n: int = 0
+        head = self.head
+        while head:
+            head = head.next
+            n += 1
+        return n
+
+    def add(self, val: int, index: int = 0) -> None:
+        """ Inserts a value node at the index, if valid. """
 
         if index > self.size or index < 0:
             # index is out of range
             return
+
         elif not index:
             # list is empty
-            new_node = ListNode(key, val, _next=self.head)
+            new_node = Node(val, next=self.head)
             self.head = new_node
-            self.size += 1
+
         else:
             # traverse list until found an insertion point
-            new_node = ListNode(key, val)
+            new_node = Node(val)
             head = self.head
             while index > 1 and head.next:
                 head = head.next
                 index -= 1
             new_node.next = head.next
             head.next = new_node
-            self.size += 1
 
     def remove(self, index: int) -> None:
         """ Removes node at the index, if valid. """
@@ -75,10 +78,11 @@ class LinkedList:
         if index < 0 or index > self.size - 1 or not self.size:
             # index is out of range or list is empty
             return
+
         elif not index:
             # remove first node
             self.head = self.head.next
-            self.size -= 1
+
         else:
             # link node following one at the index to the previous
             head = self.head
@@ -90,41 +94,41 @@ class LinkedList:
                 head.next = head.next.next
             else:
                 head.next = None
-            self.size -= 1
 
-    def get(self, index: int) -> Optional[tuple]:
-        """ Gets key-value of the node at the index, if valid. """
+    def get(self, index: int) -> Optional[int]:
+        """ Gets value of the node at the index, if valid. """
 
         if index < 0 or index > self.size - 1 or not self.size:
             # index is out of range or list is empty
             return
+
         else:
             head = self.head
             while head.next and index > 0:
                 head = head.next
                 index -= 1
-            return head.key, head.val
+            return head.val
 
-    def find(self, key: Optional[Union[int, float, str]]) -> Optional[int]:
-        """ Finds index of the node matching key, if exists. """
+    def find(self, val: int) -> Optional[int]:
+        """ Finds index of the node matching value, if exists. """
 
         head = self.head
         i = 0
         while head:
-            if head.key == key:
+            if head.val == val:
                 return i
             head = head.next
             i += 1
 
-    def to_list(self) -> list[tuple]:
-        """ Gets all key-value pairs as a list. """
+    def to_list(self) -> list[int]:
+        """ Gets all values as an array. """
 
-        kv_pairs = list()
+        vals = list()
         head = self.head
         while head:
-            kv_pairs.append((head.key, head.val))
+            vals.append(head.val)
             head = head.next
-        return kv_pairs
+        return vals
 
     def rotate(self, k: int) -> None:
         """ Rotate list by k places. """
@@ -140,7 +144,6 @@ class LinkedList:
 
         # remove redundant shifts
         k = self.size - k % self.size
-        print(k)
 
         last.next = self.head  # make cycle
         while k > 0:  # shift last by k* places
@@ -166,6 +169,7 @@ class LinkedList:
         if not self.head:
             # empty list is not a palindrom
             return False
+
         if not self.head.next:
             # a list with single element is a palindrom
             return True
@@ -218,7 +222,7 @@ class Stack(LinkedList):
     def push(self, item: int) -> None:
         """ Adds an item to the top of the stack. """
 
-        new_head = ListNode(val=item, _next=self.head)
+        new_head = Node(val=item, next=self.head)
         self.head = new_head
         self.size += 1
 
@@ -277,6 +281,54 @@ class Queue(LinkedList):
         """ Returns true if and only if the queue is empty. """
 
         raise NotImplementedError
+
+
+# Linked List OPS
+def merge_sorted(list1: LinkedList, list2: LinkedList) -> LinkedList:
+    """ Merges two sorted singly linked lists into a one sorted list.
+        NOTE: the method yields undefined result if passed unsorted list.
+        Runtime: O(N), memory: O(N).
+
+    Args:
+        list1 (LinkedList): first LL.
+        list2 (LinkedList): second LL.
+
+    Returns:
+        LinkedList: a sorted linked list.
+    """
+
+    # passed empty list1, list2, or both
+    if not list1.size:
+        return list2
+    elif not list2.size:
+        return list1
+    elif (not list1.size) and (not list2.size):
+        return LinkedList()
+
+    # transfer vals from lists until either one is exhausted
+    dummy: Node = Node()
+    head = dummy
+    h1: Node = list1.head
+    h2: Node = list2.head
+    while h1 and h2:
+        if h1.val < h2.val:
+            head.next = Node(h1.val)
+            h1 = h1.next
+        else:
+            head.next = Node(h2.val)
+            h2 = h2.next
+        head = head.next
+
+    # link tail of the non-empty list
+    if h1:
+        head.next = h1
+    elif h2:
+        head.next = h2
+
+    # transform node into LL
+    merged = LinkedList()
+    merged.head = dummy.next
+    return merged
 
 
 if __name__ == '__main__':
