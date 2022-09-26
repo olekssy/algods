94f309f964c9a08969f62360ad72fe8f5e7703d8
Olekssy Lialka <lialka.dev@protonmail.ch>
2022-09-26 11:43:39 +0200

    Added min getter to the Stack

--- data_structures/stacks.py
+++ data_structures/stacks.py
@@ -5,8 +5,16 @@ from typing import Optional
 from data_structures import linked_lists
 
 
+class Node(linked_lists.Node):
+
+    def __init__(self, val: int = 0, next=None, min=0) -> None:
+        super().__init__(val, next)
+        self.min = min
+
+
 class Stack(linked_lists.LinkedList):
-    """ A stack data structure as a linked list. Supports LIFO ordering.
+    """ A stack data structure as a linked list. Supports LIFO ordering and all
+        methods in O(N).
 
     Attributes:
         head (ListNode, None): a top node of the stack.
@@ -17,6 +25,7 @@ class Stack(linked_lists.LinkedList):
         pop(): removes the top item from the stack, if exists.
         peek(): returns the top of the stack.
         is_empty(): returns true if and only if the stack is empty.
+        min(): retrieves the minimum element in the stack.
         * other methods inherited from the parent class.
     """
 
@@ -26,7 +35,8 @@ class Stack(linked_lists.LinkedList):
     def push(self, item: int) -> None:
         """ Adds an item to the top of the stack. """
 
-        self.head = linked_lists.Node(val=item, next=self.head)
+        _min = min(self.head.min, item) if self.head else item
+        self.head = Node(val=item, next=self.head, min=_min)
         self.size += 1
 
     def pop(self) -> None:
@@ -45,3 +55,8 @@ class Stack(linked_lists.LinkedList):
         """ Returns true if and only if the stack is empty. """
 
         return not bool(self.size)
+
+    def min(self) -> Optional[int]:
+        """ Retrieves the minimum element in the stack. """
+
+        return self.head.min if self.head else None
