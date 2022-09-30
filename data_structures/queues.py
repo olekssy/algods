b346ab92b71059cea19925eb9a2e918bdf95a59b
Olekssy Lialka <dev.lialka@pm.me>
2022-10-01 00:52:34 +0200

    Added implementation of queue with stacks

--- data_structures/queues.py
+++ data_structures/queues.py
@@ -9,16 +9,16 @@ class CircularQueue:
     """ A circular queue as a linked list with a fixed size.
 
     Attributes:
-        front (int, None): Gets the front item from the queue, if not empty.
-        rear (int, None): Gets the last item from the queue, if not empty.
-        size (int): Number of elements present in the queue.
+        front (int, None): Front element in the queue, if not exists.
+        rear (int, None): Last item from the queue, if not exists.
+        size (int): Number of elements in the queue.
         capacity (int): Number of elements the queue can hold.
 
     Methods:
-        bool enqueue(val): Inserts an element into the circular queue.
-        bool dequeue(): Deletes a front element from the circular queue.
-        bool is_empty(): Checks whether the circular queue is empty or not.
-        bool is_full(): Checks whether the circular queue is full or not.
+        bool enqueue(val): Inserts an element into the queue.
+        bool dequeue(): Deletes a front element from the queue.
+        bool is_empty(): Checks whether the queue is empty.
+        bool is_full(): Checks whether the queue is full.
     """
 
     def __init__(self, capacity: int = 5) -> None:
@@ -40,7 +40,7 @@ class CircularQueue:
         return self.tail.val if not self.is_empty() else None
 
     def enqueue(self, val: int) -> bool:
-        """ Inserts an element into the circular queue.
+        """ Inserts an element into the queue.
             Returns true if the operation is successful, or false otherwise. """
 
         if self.is_full():
@@ -55,7 +55,7 @@ class CircularQueue:
         return True
 
     def dequeue(self) -> bool:
-        """ Deletes a front element from the circular queue.
+        """ Deletes a front element from the queue.
             Returns true if the operation is successful, or false otherwise. """
 
         if self.is_empty():
@@ -65,12 +65,12 @@ class CircularQueue:
         return True
 
     def is_empty(self) -> bool:
-        """ Checks whether the circular queue is empty or not. """
+        """ Checks whether the queue is empty. """
 
         return not self.size
 
     def is_full(self) -> bool:
-        """ Checks whether the circular queue is full or not. """
+        """ Checks whether the queue is full. """
 
         return self.size == self.capacity
 
@@ -79,8 +79,8 @@ class CircularDeque(CircularQueue):
     """ A circular double-ended queue as a linked list with a fixed size.
 
     Attributes:
-        front (int, None): Gets the front item from the queue, if not empty.
-        rear (int, None): Gets the last item from the queue, if not empty.
+        front (int, None): Front item from the queue, if not empty.
+        rear (int, None): Last item from the queue, if not empty.
         size (int): Number of elements present in the queue.
         capacity (int): Number of elements the queue can hold.
 
@@ -157,3 +157,64 @@ class CircularDeque(CircularQueue):
             self.tail.next = None
         self.size -= 1
         return True
+
+
+class StackQueue:
+    """ Implementation of a queue with stacks. Amortized push, pop operations
+        in O(1), if used in sequence, O(N) otherwise.
+
+    Attributes:
+        size (int): Number of elements in the queue.
+        front (int, None): Front element in the queue, if exists.
+
+    Methods:
+        null push(val): Inserts an element into the queue.
+        bool pop(): Deletes a front element from the queue.
+        int peek(): Gets the first element in queue, if exists.
+        bool is_empty(): Checks whether the queue is empty.
+    """
+
+    def __init__(self) -> None:
+        self.pop_stack: list[int] = list()
+        self.push_stack: list[int] = list()
+        self.front: Optional[int] = None
+        self.size: int = 0
+
+    def push(self, val) -> None:
+        """ Inserts an element into the queue. """
+
+        if self.pop_stack:
+            # reverse order transfer
+            while self.pop_stack:
+                self.push_stack.append(self.pop_stack.pop())
+
+        self.push_stack.append(val)
+        self.size += 1
+
+    def pop(self) -> bool:
+        """ Deletes a front element from the queue. """
+
+        if self.is_empty():
+            return False
+
+        if self.push_stack:
+            # reverse order transfer
+            while self.push_stack:
+                self.pop_stack.append(self.push_stack.pop())
+
+        self.pop_stack.pop()
+        self.size -= 1
+        return True
+
+    def peek(self) -> Optional[int]:
+        """ Gets the first element in queue, if exists. """
+
+        if self.is_empty():
+            return None
+
+        return self.push_stack[0] if self.push_stack else self.pop_stack[-1]
+
+    def is_empty(self) -> bool:
+        """ Checks whether the queue is empty. """
+
+        return not self.pop_stack and not self.push_stack
