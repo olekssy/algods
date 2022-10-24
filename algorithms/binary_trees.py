0e5a4d10f23f68fbd477e0f64511ccfa5d574f78
Olekssy Lialka <lialka.dev@protonmail.ch>
2022-10-24 13:39:50 +0200

    Added a bin tree builder from inorder, postorder; fixed traversal

--- algorithms/binary_trees.py
+++ algorithms/binary_trees.py
@@ -6,28 +6,64 @@ from data_structures import trees
 def pre_order(root: trees.BinTreeNode, vals: list[int] = list()) -> list[int]:
     """ Preorder traversal of the binary tree. """
 
-    if root:
-        vals.append(root.val)
-        pre_order(root.left, vals)
-        pre_order(root.right, vals)
+    vals = list()
+
+    def traverse(root):
+        if root:
+            vals.append(root.val)
+            traverse(root.left)
+            traverse(root.right)
+
+    traverse(root)
     return vals
 
 
 def in_order(root: trees.BinTreeNode, vals: list[int] = list()) -> list[int]:
     """ Preorder traversal of the binary tree. """
 
-    if root:
-        in_order(root.left, vals)
-        vals.append(root.val)
-        in_order(root.right, vals)
+    vals = list()
+
+    def traverse(root):
+        if root:
+            traverse(root.left)
+            vals.append(root.val)
+            traverse(root.right)
+
+    traverse(root)
     return vals
 
 
 def post_order(root: trees.BinTreeNode, vals: list[int] = list()) -> list[int]:
     """ Preorder traversal of the binary tree. """
 
-    if root:
-        post_order(root.left, vals)
-        post_order(root.right, vals)
-        vals.append(root.val)
+    vals = list()
+
+    def traverse(root):
+        if root:
+            traverse(root.left)
+            traverse(root.right)
+            vals.append(root.val)
+
+    traverse(root)
     return vals
+
+
+def from_inorder_postorder(inorder: list[int],
+                           postorder: list[int]) -> trees.BinTreeNode:
+    """ Builds a binary tree from the lists of inorder, preorder vals. """
+
+    inorder_map: dict = {v: i for i, v in enumerate(inorder)}
+    idx = 1
+
+    def build(left_id: int, right_id: int):
+        if left_id > right_id:
+            return
+        nonlocal idx
+        root = trees.BinTreeNode(postorder[-idx])
+        idx += 1
+        root_id = inorder_map[root.val]
+        root.right = build(root_id + 1, right_id)
+        root.left = build(left_id, root_id - 1)
+        return root
+
+    return build(0, len(inorder) - 1)
