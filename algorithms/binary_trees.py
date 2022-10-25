c99852e4db900cff8af81b6222e7c134398fce36
Olekssy Lialka <lialka.dev@protonmail.ch>
2022-10-25 23:08:42 +0200

    Added tree from inorder, preorder

--- algorithms/binary_trees.py
+++ algorithms/binary_trees.py
@@ -3,7 +3,7 @@
 from data_structures import trees
 
 
-def pre_order(root: trees.BinTreeNode, vals: list[int] = list()) -> list[int]:
+def pre_order(root: trees.BinTreeNode) -> list[int]:
     """ Preorder traversal of the binary tree. """
 
     vals = list()
@@ -18,8 +18,8 @@ def pre_order(root: trees.BinTreeNode, vals: list[int] = list()) -> list[int]:
     return vals
 
 
-def in_order(root: trees.BinTreeNode, vals: list[int] = list()) -> list[int]:
-    """ Preorder traversal of the binary tree. """
+def in_order(root: trees.BinTreeNode) -> list[int]:
+    """ Inorder traversal of the binary tree. """
 
     vals = list()
 
@@ -33,8 +33,8 @@ def in_order(root: trees.BinTreeNode, vals: list[int] = list()) -> list[int]:
     return vals
 
 
-def post_order(root: trees.BinTreeNode, vals: list[int] = list()) -> list[int]:
-    """ Preorder traversal of the binary tree. """
+def post_order(root: trees.BinTreeNode) -> list[int]:
+    """ Postorder traversal of the binary tree. """
 
     vals = list()
 
@@ -50,20 +50,41 @@ def post_order(root: trees.BinTreeNode, vals: list[int] = list()) -> list[int]:
 
 def from_inorder_postorder(inorder: list[int],
                            postorder: list[int]) -> trees.BinTreeNode:
-    """ Builds a binary tree from the lists of inorder, preorder vals. """
+    """ Builds a binary tree from the lists of inorder, postorder vals. """
 
     inorder_map: dict = {v: i for i, v in enumerate(inorder)}
     idx = 1
 
-    def build(left_id: int, right_id: int):
-        if left_id > right_id:
+    def build(left: int, right: int):
+        if left > right:
             return
         nonlocal idx
         root = trees.BinTreeNode(postorder[-idx])
         idx += 1
         root_id = inorder_map[root.val]
-        root.right = build(root_id + 1, right_id)
-        root.left = build(left_id, root_id - 1)
+        root.right = build(root_id + 1, right)
+        root.left = build(left, root_id - 1)
+        return root
+
+    return build(0, len(inorder) - 1)
+
+
+def from_preorder_inorder(inorder: list[int],
+                          preorder: list[int]) -> trees.BinTreeNode:
+    """ Builds a binary tree from the lists of inorder, preorder vals. """
+
+    inorder_map: dict = {v: i for i, v in enumerate(inorder)}
+    idx = 0
+
+    def build(left: int, right: int):
+        if left > right:
+            return
+        nonlocal idx
+        root = trees.BinTreeNode(preorder[idx])
+        idx += 1
+        root_id = inorder_map[root.val]
+        root.left = build(left, root_id - 1)
+        root.right = build(root_id + 1, right)
         return root
 
     return build(0, len(inorder) - 1)
