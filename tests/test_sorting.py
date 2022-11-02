df83b576b73bafbc6a72434c6c096aee00621071
Olekssy Lialka <lialka.dev@protonmail.ch>
2022-11-02 13:01:02 +0100

    Added bubble sort algorithm

--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -13,3 +13,14 @@ from algorithms import sorting as srt
 def test_selection_sort(arr: list[int], expected: list[int]):
     srt.selection_sort(arr)
     assert arr == expected
+
+
+@pytest.mark.parametrize(argnames=['arr', 'expected'],
+                         argvalues=[
+                             ([], []),
+                             ([1], [1]),
+                             ([5, 4, 3, 1, 2], [1, 2, 3, 4, 5]),
+                         ])
+def test_bubble_sort(arr: list[int], expected: list[int]):
+    srt.bubble_sort(arr)
+    assert arr == expected
