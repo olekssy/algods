a8582046998639897b23ce7833efd8daab5acb91
Olekssy Lialka <lialka.dev@protonmail.ch>
2022-11-02 13:04:55 +0100

    Added all O(1) data structure class signature

---
+++ data_structures/specialties.py
@@ -0,0 +1,20 @@
+""" Implementations of a specialty data structures. """
+
+
+class AllOne:
+    """ A data structure with all methods in O(1). """
+
+    def __init__(self):
+        pass
+
+    def inc(self, key: str) -> None:
+        pass
+
+    def dec(self, key: str) -> None:
+        pass
+
+    def getMaxKey(self) -> str:
+        pass
+
+    def getMinKey(self) -> str:
+        pass
