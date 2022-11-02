11950bbecb1a9a68f127fc5e416b01c6380bc795
Olekssy Lialka <dev.lialka@pm.me>
2022-11-02 13:01:02 +0100

    Added bubble sort algorithm

--- algorithms/sorting.py
+++ algorithms/sorting.py
@@ -11,3 +11,15 @@ def selection_sort(arr: list[int]) -> None:
                 min_index = k
 
         arr[i], arr[min_index] = arr[min_index], arr[i]
+
+
+def bubble_sort(arr: list[int]) -> None:
+    """ Sorts array values in ascending order. O(n^2), O(n). """
+
+    _sorted = False
+    while not _sorted:
+        _sorted = True
+        for i in range(len(arr) - 1):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                _sorted = False
