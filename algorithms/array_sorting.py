7c7b79e99281e2ec39eb1a3a94766f06d391cbdd
Olekssy Lialka <dev.lialka@pm.me>
2022-11-03 10:41:15 +0100

    Added insertion sort

--- algorithms/array_sorting.py
+++ algorithms/array_sorting.py
@@ -2,7 +2,7 @@
 
 
 def selection_sort(arr: list[int]) -> None:
-    """ Sorts array values in ascending order. O(n^2), O(1). """
+    """ Sorts array inplace. Unstable sort. O(n^2), O(1). """
 
     for i in range(len(arr)):
         min_index = i
@@ -14,7 +14,7 @@ def selection_sort(arr: list[int]) -> None:
 
 
 def bubble_sort(arr: list[int]) -> None:
-    """ Sorts array values in ascending order. O(n^2), O(n). """
+    """ Sorts array inplace. Stable sort. O(n^2), O(1). """
 
     _sorted = False
     while not _sorted:
@@ -23,3 +23,15 @@ def bubble_sort(arr: list[int]) -> None:
             if arr[i] > arr[i + 1]:
                 arr[i], arr[i + 1] = arr[i + 1], arr[i]
                 _sorted = False
+
+
+def insertion_sort(arr: list[int]) -> None:
+    """ Sorts array inplace. Stable sort. O(n^2), O(1).
+        Preferred for almost sorted or small arrays. """
+
+    for i in range(1, len(arr)):
+        curr_id = i
+
+        while curr_id > 0 and arr[curr_id - 1] > arr[curr_id]:
+            arr[curr_id], arr[curr_id - 1] = arr[curr_id - 1], arr[curr_id]
+            curr_id -= 1
