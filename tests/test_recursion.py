ec28c334efa65c72c6ff4c4bfa84780b7630b3aa
Olekssy Lialka <dev.lialka@pm.me>
2022-11-14 12:13:29 +0100

    Added Pascal's triangle with recursion

--- tests/test_recursion.py
+++ tests/test_recursion.py
@@ -14,3 +14,17 @@ from algorithms import recursion
 def test_reverse_arr(arr: list[int], expected: list[int]):
     recursion.reverse_arr(arr)
     assert arr == expected
+
+
+@pytest.mark.parametrize(argnames=['depth', 'expected'],
+                         argvalues=[
+                             (-3, []),
+                             (0, [[1]]),
+                             (1, [[1], [1, 1]]),
+                             (2, [[1], [1, 1], [1, 2, 1]]),
+                             (3, [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1]]),
+                             (5, [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1],
+                                  [1, 4, 6, 4, 1], [1, 5, 10, 10, 5, 1]]),
+                         ])
+def test_pascals_triangle(depth: int, expected: list[list[int]]):
+    assert recursion.pascals_triangle(depth) == expected
