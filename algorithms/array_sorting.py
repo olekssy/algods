e11a7eca38d7a67d6575c354a2ec71cdbbc4ec6c
Olekssy Lialka <dev.lialka@pm.me>
2022-11-17 13:07:44 +0100

    Added merge sorting algo

--- algorithms/array_sorting.py
+++ algorithms/array_sorting.py
@@ -35,3 +35,55 @@ def insertion_sort(arr: list[int]) -> None:
         while curr_id > 0 and arr[curr_id - 1] > arr[curr_id]:
             arr[curr_id], arr[curr_id - 1] = arr[curr_id - 1], arr[curr_id]
             curr_id -= 1
+
+
+def merge_sort(arr: list[int]) -> list[int]:
+    """ Sorts array with recursive, top-down merge sort algorithm.
+        Stable sort in O(n logn), O(n). """
+
+    if len(arr) < 2:
+        return arr
+
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    return _merge_sorted(left, right)
+
+
+def merge_sort_alt(arr: list[int]) -> list[int]:
+    """ Sorts array with recursive, bottom-up merge sort algorithm.
+        Stable sort in O(n logn), O(n). """
+
+    if len(arr) < 2:
+        return arr
+
+    val_splits = [[n] for n in arr]
+
+    while len(val_splits) > 1:
+        ordered = list()
+        for i in range(0, len(val_splits), 2):
+            # i + 1 can be out of range
+            right = val_splits[i + 1] if i + 1 < len(val_splits) else list()
+            ordered.append(_merge_sorted(val_splits[i], right))
+            i += 2
+        val_splits = ordered
+
+    return val_splits[0]
+
+
+def _merge_sorted(l1: list[int], l2: list[int]) -> list[int]:
+    """ Merges two sorted arrays. """
+
+    p1 = p2 = 0
+    merged = list()
+
+    while p1 < len(l1) and p2 < len(l2):
+        if l1[p1] < l2[p2]:
+            merged.append(l1[p1])
+            p1 += 1
+        else:
+            merged.append(l2[p2])
+            p2 += 1
+
+    merged += l1[p1:] + l2[p2:]
+    return merged
